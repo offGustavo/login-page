8da9eb2c01c26873311524b3f0a031fe0898aa3b
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2024-02-01 00:45:58 +0000

    addJs

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById("btnLoginJs");
+
+
+btn.addEventListener('click', ()=>{
+ document.getElementById("password").value = " ";
+ document.getElementById("email").value = " ";
+ document.getElementById("email").focus();
+})
