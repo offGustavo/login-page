468a12e74b379d9290945e050a556a4d22c19e82
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2024-01-31 21:51:59 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let btn = document.getElementById("btnLoginJs");
 
 
 btn.addEventListener('click', ()=>{
- document.getElementById("password").value = " ";
- document.getElementById("email").value = " ";
+ document.getElementById("password").value = "";
+ document.getElementById("email").value = "";
  document.getElementById("email").focus();
 })
